d43b6a2e2e7d8322c077efdc4de3e3e86eef6a67
XCR67 <33xaviercamacho@gmail.com>
2024-08-22 01:21:19 -0700

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,30 @@
+#CAESAR CIPHER
+#Encrypts message by shifting the letters by a given number
+def encrypt(msg, shift):
+  alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  result = ""
+  for i in range(len(msg)): #Go through each letter
+    if msg[i].upper() in alpha: #If its a letter
+      pos = alpha.index(msg[i].upper())
+      result += alpha[(pos + shift) % 26] #Add the shifted letter to the result
+    else:
+      result += msg[i] #Add the space / punctuation to the result
+  return result
+#Decrypts my shifting back the letters by a given number
+def decrypt(msg, shift):
+  alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  result = ""
+  for i in range(len(msg)): #Go through each letter
+    if msg[i].upper() in alpha: #If its a letter
+      pos = alpha.index(msg[i].upper()) #Get the position of the letter in alpha
+      if pos - shift >= 0: #If it didnt loop around (d to a)
+        result += alpha[(pos - shift)]
+      else: #If it did loop around (z to c)
+        result += alpha[(pos - shift) + 26]
+    else:
+      result += msg[i] #Add it to the result
+  return result
+#Caesar Cipher in action
+message = encrypt("Hello World!", 15)
+print(message)
+print(decrypt(message, 15))
