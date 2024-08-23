9a78d781efd646c2deb4d1f1956c2629b6c63a69
XCR67 <33xaviercamacho@gmail.com>
2024-08-23 01:01:31 -0700

    Caesar Cipher Bones.py

--- main (4) (1).py
+++ main (4) (1).py
@@ -1,50 +1,50 @@
 #DAY 2 - CAESAR CIPHER
 #Encrypts message by shifting the letters by a given number
-def encrypt(msg, shift):
-  alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  result = ""
-  for i in range(len(msg)): #Go through each letter
-    if msg[i].upper() in alpha: #If its a letter
-      pos = alpha.index(msg[i].upper())
-      result += alpha[(pos + shift) % 26] #Add the shifted letter to the result
-    else:
-      result += msg[i] #Add the space / punctuation to the result
-  return result
-#Decrypts my shifting back the letters by a given number
-def decrypt(msg, shift):
-  alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  result = ""
-  for i in range(len(msg)): #Go through each letter
-    if msg[i].upper() in alpha: #If its a letter
-      pos = alpha.index(msg[i].upper()) #Get the position of the letter in alpha
-      if pos - shift >= 0: #If it didnt loop around (d to a)
-        result += alpha[(pos - shift)]
-      else: #If it did loop around (z to c)
-        result += alpha[(pos - shift) + 26]
-    else:
-      result += msg[i] #Add it to the result
-  return result
-#Caesar Cipher in action
-message = encrypt("Hello World!", 15)
-print(message)
-print(decrypt(message, 15))
+#def encrypt(msg, shift):
+  #Variable initilization, need string with the alphabet and a result string
+
+  #Loop through each letter
+    #If its a letter
+      #Add the shifted letter to the result
+    #If its something else like punctuation
+      #Add the space / punctuation to the result
+  #Return the result
+
+
+#Decrypts by shifting back the letters by a given number
+#def decrypt(msg, shift):
+  #Variable initilization, need string with the alphabet and a result string
+
+  #Loop through each letter
+    #If its a letter
+      #Get the position of the letter in alpha
+        #If it didnt loop around (d to a)
+          #subtract the pos by shift
+        #If it did loop around (z to c) 
+          #subtract also, but add 26
+
+    #Add it to the result
+  #Return the result
+
+
+
 
 #ASCII CAESAR CIPHER
 #Encrypts message by shifting the ascii by a given number
-def encryptAscii(msg, shift):
-  result = ""
-  for i in range(len(msg)): #Go through each letter
+#def encryptAscii(msg, shift):
+  #Create a result string
+
+  #Loop through each character
     #Add shifted ascii letter to result
-    result += chr(ord(msg[i]) + shift)
-  return result
+
+  #Return result
+
+
 #Decrypts by shifting back the ascii value by a given number
-def decryptAscii(msg, shift):
-  result = ""
-  for i in range(len(msg)): #Go through each letter
+#def decryptAscii(msg, shift):
+  #Create a result string
+
+  #Loop through each letter
     #Add reverted ascii letter to result
-    result += chr(ord(msg[i]) - shift)
-  return result
-#Caesar Cipher Ascii in action
-message = encryptAscii("Hello World!", 15)
-print(message)
-print(decryptAscii(message, 15))
+
+  #Return the result
